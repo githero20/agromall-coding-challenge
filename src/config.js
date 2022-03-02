4936fefade36ab0fa0c7db2a30c9147d6311c97a
Omogbai Atakpu <atakpuomogbai@gmail.com>
2022-03-02 18:24:18 +0100

    my test solution

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+  api: {
+    authUrl: 'https://accounts.spotify.com/api/token',
+    baseUrl: 'https://api.spotify.com/v1',
+    clientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
+  }
+}
